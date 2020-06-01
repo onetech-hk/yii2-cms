e104da79b195fa15383d6a9963181de47d2c9138
Ken Wong <ken.wong@101medialab.com>
2020-06-01 14:55:21 +0800

    Create Module

---
+++ Module.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace onetech\cms;
+
+use Yii;
+
+/**
+ * @package onetech\cms
+ */
+class Module extends \yii\base\Module
+{
+    /**
+     * {@inheritdoc}
+     */
+    public $controllerNamespace = 'onetech\cms\controllers';
+
+    /**
+     * {@inheritdoc}
+     */
+    public function init()
+    {
+        parent::init();
+    }
+}
